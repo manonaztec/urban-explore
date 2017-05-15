065086b1d233e7c28d40306a2dc7f30b777ae67c
manon chauvin <manonlz.chauvin@gmail.com>
2017-05-15 16:36:11 +0200

    add booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515142811) do
+ActiveRecord::Schema.define(version: 20170515143533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "engine_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["engine_id"], name: "index_bookings_on_engine_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
 
   create_table "engines", force: :cascade do |t|
     t.string   "category"
@@ -52,5 +61,7 @@ ActiveRecord::Schema.define(version: 20170515142811) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "engines"
+  add_foreign_key "bookings", "users"
   add_foreign_key "engines", "shops"
 end
