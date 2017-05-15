7bd6c4747371bb9a6f7c8cfafc56e0b81c669836
manon chauvin <manonlz.chauvin@gmail.com>
2017-05-15 16:44:17 +0200

    routes creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :engines, only: [:index, :show] do
+    resources :booking, only: [:index], shallow: true
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
